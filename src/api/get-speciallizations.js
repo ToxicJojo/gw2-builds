9da5aeb2dbad7032b932698de8a8e38724789a12
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-10-11 19:53:23 +0200

    Added specializations api

---
+++ src/api/get-speciallizations.js
@@ -0,0 +1,10 @@
+import config from './config.json'
+
+const getTraits = async (ids, language = config.defaultLanguage) => {
+  const response = await fetch(`${config.baseURL}/specializations?ids=${ids}&lang=${language}`)
+  const traits = await response.json()
+
+  return traits
+}
+
+export default getTraits
--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import getTraits from './get-traits'
+import getSpecializations from './get-speciallizations'
 
 export default {
   getTraits,
+  getSpecializations,
 }
