ba58fc185f3ed68d85b45bf8a1cd03fad53d1bb6
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-10-12 16:52:54 +0200

    Profession api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import getTraits from './get-traits'
 import getSpecializations from './get-speciallizations'
+import getProfessions from './get-professions'
 
 export default {
   getTraits,
   getSpecializations,
+  getProfessions,
 }
