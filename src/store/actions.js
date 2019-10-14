3c002fc931e9d11a5aad3ea02df12d5a9d618701
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-10-14 13:07:47 +0200

    Profession store

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,6 @@
 import api from '@/api'
 
+// TODO abstract this logic into one function. Or creating a factory.
 const actions = {
   async loadTraits ({ state, commit }, ids) {
     // We only need to load ids that are not already loaded.
@@ -23,6 +24,18 @@ const actions = {
     if (notLoadedIds.length >= 1) {
       const specializations = await api.getSpecializations(notLoadedIds)
       commit('addSpecializations', specializations)
+    }
+  },
+  async loadProfessions ({ state, commit }, ids) {
+    // We only need to load ids that are not already loaded.
+    const notLoadedIds = ids.filter((id) => {
+      return state.professions[id] === undefined
+    })
+
+    // Don't hit the api if there are no new traits to load.
+    if (notLoadedIds.length >= 1) {
+      const professions = await api.getProfessions(notLoadedIds)
+      commit('addProfessions', professions)
     }
   },
 }
