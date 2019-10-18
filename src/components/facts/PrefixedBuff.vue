28e34001360080c6036b0dac5a3d915b8a83ed3e
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-10-18 21:15:20 +0200

    Added prefixed buff

---
+++ src/components/facts/PrefixedBuff.vue
@@ -0,0 +1,27 @@
+<template lang='pug'>
+  .prefixed-buff
+    img(:src='fact.prefix.icon')
+    span {{ fact.prefix.status }}
+</template>
+
+<script>
+export default {
+  name: 'PrefixedBuff',
+  props: ['fact'],
+}
+</script>
+
+<style lang="scss" scoped>
+
+.prefixed-buff {
+  display: flex;
+  align-items: center;
+}
+
+img {
+  width: 32px;
+  height: 32px;
+  margin: 4px;
+}
+
+</style>
